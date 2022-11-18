2720abc0b1aac94329f19eb482a8c4b6373738c7
Dixon <Kianush.Dixon@tivityhealth.com>
2022-11-17 22:56:34 -0600

    REdux store - confusing

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,23 +1,25 @@
+import { useSelector } from 'react-redux';
 import { Col, Row } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from './campsitesSlice';
 
 const CampsitesList = () => {
     // selectAllCampsites is a function in campsitesSlice...
-    const campsites = selectAllCampsites();
+    const campsites = useSelector(selectAllCampsites);
+    console.log('campsites:', campsites);
     return (
         <Row className='ms-auto'>
             {campsites.map((campsite1) => {
                 return (
-                <Col
-                    md='5'
-                    className='m-4'
-                    key={campsite1.id}
-                >
-                    <CampsiteCard campsite={campsite1} />
+                    <Col
+                        md='5'
+                        className='m-4'
+                        key={campsite1.id}
+                    >
+                        <CampsiteCard campsite={campsite1} />
                     </Col>
-                    );
-        })}
+                );
+            })}
         </Row>
     );
 };
--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,18 +1,29 @@
+import { useSelector } from 'react-redux';
 import { Col, Row } from 'reactstrap';
-import DisplayCard from './DisplayCard';
+//import DisplayCard from './DisplayCard';
+import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
+import { selectFeaturedPartner } from '../partners/partnersSlice';
 
 const DisplayList = () => {
-    const items = [selectFeaturedCampsite(), selectFeaturedPromotion()];
+    const items = useSelector((state) =>[
+        selectFeaturedCampsite(state), 
+        selectFeaturedPromotion(state), 
+        selectFeaturedPartner(state)
+    ]);
 
-    return(
+    console.log('display items:', items);
+    
+    return (
         <Row>
-            {items.map((item, idx) => { return (
-                <Col md className='m-1' key={idx}>
-                    <DisplayCard item={item} />
-                </Col>
-            );}
+            {items.map((item, idx) => {
+                return item && (  // only if there is a featured item, display it.
+                    <Col md className='m-1' key={idx}>
+                        <AnimatedDisplayCard item={item} />
+                    </Col>
+                );
+            }
             )}
         </Row>
     );
